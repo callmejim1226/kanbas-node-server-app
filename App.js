de1426d057a0b61f3d0a4e0f195e1885c22ec979
Ji-min Kim <kimjimin1226@gmail.com>
2024-03-29 21:41:51 -0400

    FIX: removed restrictions in App.js

--- App.js
+++ App.js
@@ -1,41 +1,23 @@
 // const express = require("express");
 import express from "express";
 import Hello from "./Hello.js";
-import Courses from "./Courses/routes.js";
-import Modules from "./Modules/routes.js";
 import cors from "cors";
-import session from "express-session";
-import SessionExercises from "./SessionExercises.js";
-import Users from "./Users/routes.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
 
 CourseRoutes(app);
 ModuleRoutes(app);
 
 Hello(app);
 Lab5(app);
-Courses(app);
-Modules(app);
-SessionExercises(app);
-Users(app);
+// Courses(app);
+// Modules(app);
+// SessionExercises(app);
+// Users(app);
 
 app.listen(process.env.PORT || 4000);
